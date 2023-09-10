eea73d7e8f5ca73174bd83afcdc0bc5fdffc923d
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-09-10 07:37:01 +0530

    Initial Setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("API is listening");
+});
+app.listen(PORT, () => {
+  console.log(`Server is listening in Port ${PORT}`);
+});
