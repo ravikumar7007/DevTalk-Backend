9e8d1cddacf0c730e01eebf7b6e84b6a1a5722c1
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-09-16 07:50:47 +0530

    install cors package

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT;
+const cors = require("cors");
+app.use(cors());
 app.use(express.json({ extended: false }));
 connectDB();
 
